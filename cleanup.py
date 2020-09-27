d6fcb87c0d82452f827a5e635760ae17896bd64a
Aswin Karuvally <aswinbabuk@gmail.com>
2020-09-27 16:25:39 +0530

    completed generate_list()

--- cleanup.py
+++ cleanup.py
@@ -3,15 +3,29 @@
 
 # Import serious stuff
 import os
-import datetime
+import calendar
+from datetime import datetime
+import pdb
 
 
 # Generate list of files to be deleted
 def generate_list(start_path):
+    # List of files to be removed
+    remove_list = []
+
     # Generate a datetime object
-    date_obj = datetime.datetime.now()
+    current_time = datetime.now()
 
     for root, directories, files in os.walk(start_path):
         for filename in files:
             file_path = os.path.join(root, filename)
+            modified_time = datetime.fromtimestamp(
+                os.path.getmtime(file_path)
+            )
+            file_age = current_time - modified_time
+            if file_age.days > 90:
+                remove_list.append(file_path)
             
+
+
+generate_list("/home/sreedevi")
